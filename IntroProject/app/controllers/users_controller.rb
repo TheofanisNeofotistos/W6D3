4614d140f2f4d826f112a0d3ada1e84f1501b46c
Michelle <rphmichelle@gmail.com>
2023-06-07 16:31:23 -0400

    working on route controller

---
+++ IntroProject/app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+# app/controllers/users_controller.rb
+
+class UsersController < ApplicationController
+    def index 
+        render plain: "I'm in the index action!"
+    end 
+
+    def create
+        render json: params
+    end 
+
+    def show 
+        render json: params
+    end 
+
+    def delete 
+        render json: params
+    end 
+
+    def update
+        render json: params
+    end 
+end
